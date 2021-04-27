b69f3a95bb9ea0ded9f92ec2e06864a532697c58
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-04-27 19:30:28 -0300

    feat: httpClient implements with axios

---
+++ infra/axios.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from "../data/contracts/http-client";
+import { api } from "../main/config/axios";
+
+export class Axios implements HttpClient {
+
+    constructor(private readonly route: string) { }
+    async post(body: any, headers?: any): Promise<any> {
+        try {
+            const response = await api.post(this.route, body, headers)
+            return response.data
+        } catch (error) {
+            return new Error(error.response.message)
+        }
+    }
+}
