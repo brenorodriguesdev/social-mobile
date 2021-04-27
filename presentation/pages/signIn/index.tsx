a4e79d405b64965b51a6bf6082df260e1e3415bb
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-04-27 18:37:44 -0300

    feat: add signIn page

---
+++ presentation/pages/signIn/index.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+import {
+    View,
+    Text,
+    TouchableOpacity,
+    StatusBar
+} from 'react-native';
+
+import {
+    TextInput,
+} from 'react-native-paper';
+
+import styles from './styles'
+
+export default function SignIn() {
+
+
+    const { container, input, button, textButton } = styles;
+    return (
+        <>
+            <StatusBar barStyle="dark-content" backgroundColor="#FFF" />
+            <View style={container}>
+                <TextInput
+                    mode="outlined"
+                    label='Email'
+                    style={input}
+                    theme={{ colors: { primary: "#0080ff" } }}
+                    keyboardType="email-address"
+                />
+                <TextInput
+                    mode="outlined"
+                    label='Senha'
+                    style={input}
+                    secureTextEntry={true}
+                    theme={{ colors: { primary: "#0080ff" } }}
+                    textContentType="password"
+                />
+                <TouchableOpacity style={button}>
+                    <Text style={textButton}>Entrar</Text>
+                </TouchableOpacity>
+                <View style={{ flexDirection: 'row' }}>
+                    <TouchableOpacity style={{marginRight:12}}>
+                        <Text>Esqueceu sua senha?</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Text>Criar Conta</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </>
+    );
+}
