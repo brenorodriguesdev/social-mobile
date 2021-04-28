bee487ba4192cb84270cd4d9bb3f45b302c60aa6
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-04-27 22:44:39 -0300

    feat: add signUp validation factory

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { makeSignInService } from './main/factories/services/sign-in-service-factory';
 import { makeSignInValidation } from './main/factories/validation/sign-in-validation-factory';
+import { makeSignUpValidation } from './main/factories/validation/sign-up-validation-factory';
 import { SignIn, SignUp } from './presentation/pages'
 
 export default function App() {
   return (
-    <SignIn signInUseCase={makeSignInService()} validator={makeSignInValidation()} />
+    //<SignIn signInUseCase={makeSignInService()} validator={makeSignInValidation()} />
+    <SignUp validator={makeSignUpValidation()} />
   );
 }
