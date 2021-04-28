56ad189a93b19f4e20dcda656c4e84578b30a813
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-04-28 11:24:22 -0300

    feat: add UserList component

---
+++ presentation/components/UserList/index.tsx
@@ -0,0 +1,25 @@
+import { TouchableOpacity, Text, View, Dimensions } from "react-native"
+import React from 'react';
+import { EvilIcons, FontAwesome5 } from "@expo/vector-icons";
+import { UserRow } from "../UserRow";
+
+interface User {
+    id: number
+    name: string
+}
+
+interface UserListProps {
+    users: User[]
+}
+
+export function UserList({ users }: UserListProps) {
+    return (
+        <>
+            <Text style={{ color: "black", fontSize: 24, fontWeight: 'bold', alignSelf: 'flex-start', marginLeft: 36 }}>Resultados</Text>
+            <View style={{ height: Dimensions.get('window').height * 60 / 100 }}>
+                {users.map(user => <UserRow key={user.id} name={user.name} />)}
+            </View>
+        </>
+    );
+
+}
