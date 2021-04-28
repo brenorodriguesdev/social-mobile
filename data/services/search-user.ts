12758aa89e508aa1d53b7e184d35640fb4f73596
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-04-28 17:30:33 -0300

    feat: inject loadAccessToken on SearchUserService

--- data/services/search-user.ts
+++ data/services/search-user.ts
@@ -1,12 +1,14 @@
 import { UserModel } from "../../domain/models/user";
 import { SearchUserUseCase } from "../../domain/useCases/search-user";
 import { HttpClient } from "../contracts/http-client";
+import { LoadAccessToken } from "../contracts/load-access-token";
 
 export class SearchUserService implements SearchUserUseCase {
-    constructor(private readonly httpClient: HttpClient) { }
+    constructor(private readonly httpClient: HttpClient, private readonly loadAccessToken: LoadAccessToken) { }
 
     async search(user: string): Promise<UserModel[]> {
-        const users = await this.httpClient.get({ user })
+        const accessToken = await this.loadAccessToken.load()
+        const users = await this.httpClient.get({ user }, accessToken)
         return users
     }
 }
