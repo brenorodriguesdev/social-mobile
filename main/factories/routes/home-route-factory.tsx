d60c394b9b4c8039e7eeac6c430389cbccd05fe9
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-04-29 21:01:21 -0300

    feat: inject get count notification service on homepage

--- main/factories/routes/home-route-factory.tsx
+++ main/factories/routes/home-route-factory.tsx
@@ -1,13 +1,14 @@
-import { Home } from "../../../presentation/pages/home";
-import { makeSearchUserService } from '../services/search-user-service-factory';
 import React from 'react'
-import { makeGetInviteListService } from "../services/get-invite-list-service-factory";
+import { Home } from "../../../presentation/pages/home";
 import { HomeProvider } from "../../../presentation/contexts/home";
+import { makeGetInviteListService } from "../services/get-invite-list-service-factory";
+import { makeSearchUserService } from '../services/search-user-service-factory';
+import { makeGetCountNotificationService } from "../services/get-count-notification-service-factory";
 
 export function makeHomeRoute(props: any) {
     return (
         <HomeProvider>
-            <Home {...props} searchUserUseCase={makeSearchUserService()} getInviteListUseCase={makeGetInviteListService()} />
+            <Home {...props} searchUserUseCase={makeSearchUserService()} getInviteListUseCase={makeGetInviteListService()} getCountNotificationUseCase={makeGetCountNotificationService()} />
         </HomeProvider>
     )
 }
